b0249b8af93d160111ec84163496f39152ae1503
lawr3ncey <babelonia.l.bscs@gmail.com>
2026-02-09 12:29:37 -0500

    Add step defs for button clicks and flash messages

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -105,6 +105,23 @@ def step_impl(context, element_name):
 ##################################################################
 
 ## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    """Click a button based on the naming convention <button>-btn"""
+    button_id = button.lower() + '-btn'
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.element_to_be_clickable((By.ID, button_id))
+    )
+    element.click()
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    """Check the flash message area for a message"""
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, 'flash_message'))
+    )
+    assert(message in element.text)
+
 
 ##################################################################
 # This code works because of the following naming convention:
